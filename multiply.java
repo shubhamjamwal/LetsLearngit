42be35de786645cfde19aa76436a819dc6d16181
shubham <shubhamthakur1415@gmail.com>
2021-09-12 13:44:55 +0530

    added multiply.java file

---
+++ multiply.java
@@ -0,0 +1,10 @@
+public class multiply {
+
+   public static void main(String[] args) {
+        
+      int num1 = 5, num2 = 15, mul;
+      mul = num1 * num2;
+
+      System.out.println("multiply of two number is "+mul);
+   }
+}
